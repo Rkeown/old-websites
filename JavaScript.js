1b3388f1d416e6c9c4ff871e1c3e7044580bf4cf
Rkeown <36601780+Rkeown@users.noreply.github.com>
2020-01-10 09:50:06 -0600

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,11 @@
+﻿/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("navBar").style.width = "250px";
+    document.getElementById("classContent").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("navBar").style.width = "0";
+    document.getElementById("classContent").style.marginLeft = "0";
+}
